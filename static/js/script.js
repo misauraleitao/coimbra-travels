8f3d1c74ba6de4519cb0b2878891d2800af29c97
misauraleitao <misauraleitao@gmail.com>
2021-10-04 08:23:39 +0000

    Added all input fields on add place page

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,13 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.slider').slider();
     $('select').formSelect();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: "Select"
+      }
+    });
   });
   
